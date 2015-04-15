acc6a4c555fda724502a20ce109f3bb342be7167
yangjl0930@gmail.com <yangjl0930@gmail.com>
2015-04-14 23:52:00 -0700

    failed to install tasselr step2

---
+++ profiling/1.SeeD_GBS/1.B.1_hdf5.R
@@ -0,0 +1,17 @@
+### Jinliang Yang
+### April 14th, 2015
+
+
+allzea <- read.csv("data//AllZeaGBSv2.7_publicSamples_metadata20140411.csv")
+table(allzea$GermplasmSet)
+table(allzea$Project)
+
+
+
+install.packages("devtools")
+install.packages("methods")
+install.packages("Rcpp")
+
+install_github("vsbuffalo/tasselr")
+
+library("devtools")
