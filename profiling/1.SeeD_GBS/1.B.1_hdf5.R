01e9952f08f01560ee190e8c8b300532179edfad
yangjl0930@gmail.com <yangjl0930@gmail.com>
2015-04-16 12:26:35 -0700

    run seeds.

--- profiling/1.SeeD_GBS/1.B.1_hdf5.R
+++ profiling/1.SeeD_GBS/1.B.1_hdf5.R
@@ -3,6 +3,39 @@
 
 
 allzea <- read.csv("data//AllZeaGBSv2.7_publicSamples_metadata20140411.csv")
+
+table(allzea$Project)
+
+table(allzea$GermplasmSet)
+
+ames <- subset(allzea, GermplasmSet %in% "Ames" & Project != "Imputation Test")
+write.table(ames$FullName, "data/Taxa_ames_3324.txt", row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+
+table(ames$Project)
+
+codechunk <- paste("module load gcc jdk/1.8 tassel/5",
+                   "run_pipeline.pl -h5 /group/jrigrp4/AllZeaGBSv2.7impV5/ZeaGBSv27_publicSamples_imputedV5_AGPv2-150114.h5 
+-export /group/jrigrp4/AllZeaGBSv2.7impV5/ZeaGBSv27_publicSamples_imputedV5_AGPv2-150114_ames.hmp -exportType Hapmap 
+-includeTaxaInfile data/Taxa_ames_3324.txt",
+                   sep="\n")
+
+
+source("~/Documents/Github/zmSNPtools/Rcodes/setUpslurm.R")
+
+setUpslurm(slurmsh="slurm-scripts/run_h5_hmp.sh",
+           codesh= codechunk,
+           wd=NULL, jobid="hdf5-hmp", email=TRUE)
+
+
+
+
+
+
+
+
+
+#######
+allzea <- read.csv("data//AllZeaGBSv2.7_publicSamples_metadata20140411.csv")
 table(allzea$GermplasmSet)
 table(allzea$Project)
 
@@ -15,4 +48,22 @@ install.packages("Rcpp")
 library("devtools")
 
 install_github("vsbuffalo/tasselr")
+
+
+library("tasselr")
+gbs <- initTasselHDF5("/group/jrigrp4/AllZeaGBSv2.7impV5/ZeaGBSv27_publicSamples_imputedV5_AGPv2-150114.h5")
+
+gbs2 <- loadBiallelicGenotypes(gbs)
+
+
+module load gcc jdk/1.8 tassel/5
+#run_pipeline.pl -Xmx64g -fork1 
+run_pipeline.pl -hdf5Schema /group/jrigrp4/AllZeaGBSv2.7impV5/ZeaGBSv27_publicSamples_imputedV5_AGPv2-150114.h5 -export /group/jrigrp4/AllZeaGBSv2.7impV5/schema.txt
+
+
+-­includeTaxaInFi efile
+
+
+module load gcc jdk/1.8 tassel/5
+run_pipeline.pl -Xmx64g -fork1 -SortGenotypeFilePlugin -inputFile ZeaGBSv27_publicSamples_imputedV5_AGPv2-150114.h5 -outputFile sortedGBS -fileType Hapmap -runfork1
 
---
+++ slurm-scripts/run_h5_hmp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -D /Users/yangjl/Documents/Github/SeeDs
+#SBATCH -o /Users/yangjl/Documents/Github/SeeDs/slurm-log/testout-%j.txt
+#SBATCH -e /Users/yangjl/Documents/Github/SeeDs/slurm-log/err-%j.txt
+#SBATCH -J hdf5-hmp
+set -e
+set -u
+
+module load gcc jdk/1.8 tassel/5
+run_pipeline.pl -h5 /group/jrigrp4/AllZeaGBSv2.7impV5/ZeaGBSv27_publicSamples_imputedV5_AGPv2-150114.h5 
+-export /group/jrigrp4/AllZeaGBSv2.7impV5/ZeaGBSv27_publicSamples_imputedV5_AGPv2-150114_ames.hmp -exportType Hapmap 
+-includeTaxaInfile data/Taxa_ames_3324.txt
+
+python /home/jolyang/bin/send_email.py -s slurm-scripts/run_h5_hmp.sh
